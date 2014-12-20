a8a33da291e426766a8dac1bbede73b9dfa5a32d
salilgupta1 <salil.gupta323@gmail.com>
2014-12-20 02:51:00 -0500

    reform logic flow

--- app.py
+++ app.py
@@ -1,16 +1,46 @@
 from flask import Flask, session, redirect, url_for, render_template, request
-from controllers.UserAuth import UserAuth
-import os
+from controllers.UserAuthController import UserAuthController
+from controllers.AnalyticsController import AnalyticsController
+import os, pprint
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTING'])
 
-userAuth = UserAuth()
-
+userAuth = UserAuthController()
+analytics = AnalyticsController()
+# home page
 @app.route('/')
 def index():
-	return render_template('index.html')
+	if 'logged_in' in session and session['logged_in'] == True:
+		return redirect(url_for('dashboard'))
+	else:
+		return render_template('index.html',client_id=os.environ['client_id'])
 
+#### Analytics pages
+@app.route('/dashboard', methods=['GET','POST'])
+def dashboard():
+	return render_template('dashboard.html')
+
+@app.route('/setup', methods=['GET','POST'])
+def setup():
+	if 'logged_in' in session and session['logged_in'] == True:
+		analytics.setUsername(session['username'])
+
+		pullError = analytics.pullData(True)
+		if pullError == True:
+			pprint.pprint(analytics.rawData)
+			refineError= analytics.refineData()
+			if refineError == True:
+				pprint.pprint(analytics.cleanData)
+				insertError = analytics.insertData()
+				if insertError == True:
+					print "Complete!"
+
+	else:
+		return redirect(url_for('index'))
+	return render_template('setup.html')
+
+# user pages
 @app.route('/auth/registerUser', methods=['GET','POST'])
 def registerUser():
 	if request.method == 'POST':
@@ -22,8 +52,10 @@ def registerUser():
 			# clear out sessions
 			session.pop('logged_in', None)
 			session.pop('username', None)
+			session['logged_in'] = True
+			session['username'] = username
 			# go to the users dashboard
-			return render_template('dashboard.html')
+			return redirect(url_for('setup'))
 		else:
 			return result
 	else:
@@ -44,7 +76,7 @@ def login():
 		if is_authenticated == True:
 			session['logged_in'] = True
 			session['username'] = username
-			return render_template('dashboard.html')
+			return redirect(url_for('dashboard'))
 		else:
 			error = is_authenticated
 	return render_template('login.html', error=error)
