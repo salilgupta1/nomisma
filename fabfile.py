9905ad096de3b9d5f2917642b8cddc596fd76648
salilgupta1 <salil.gupta323@gmail.com>
2014-12-20 05:16:32 -0500

    host added for deploy

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,8 @@
-from fabric.api import local, run
+from fabric.api import local, run, hosts
 import os
 
+
+@hosts(['ec2-23-23-210-37.compute-1.amazonaws.com'])
 def deploy():
 	local('heroku maintenance:on')
 	local('git push heroku master')
