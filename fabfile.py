b1e62ef1f054bddb6fdc27910fdc06728be1f4d1
salilgupta1 <salil.gupta323@gmail.com>
2014-12-26 18:49:46 -0500

    remove db updates

--- fabfile.py
+++ fabfile.py
@@ -6,6 +6,6 @@ import os
 def deploy():
 	local('heroku maintenance:on')
 	local('git push heroku master')
-	run('psql -d %s -af User.sql' % (os.environ['DATABASE_URL'],))
-	run('psql -d %s -af Transaction_Analytics.sql' % (os.environ['DATABASE_URL'],))
+	#run('psql -d %s -af User.sql' % (os.environ['DATABASE_URL'],))
+	#run('psql -d %s -af Transaction_Analytics.sql' % (os.environ['DATABASE_URL'],))
 	local('heroku maintenance:off')
