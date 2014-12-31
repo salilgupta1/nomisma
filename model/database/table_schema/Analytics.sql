4d8e9f34fa0062973ad1cc625339c4b529c33644
salilgupta1 <salil.gupta323@gmail.com>
2014-12-31 01:52:19 -0500

    datatype change b/c money is deprecated

--- model/database/table_schema/Analytics.sql
+++ model/database/table_schema/Analytics.sql
@@ -5,11 +5,11 @@ CREATE TABLE IF NOT EXISTS "analytics"(
 		ON DELETE CASCADE,
 	num_trans_inflow INT NOT NULL,
 	num_trans_outflow INT NOT NULL, 
-	inflow Money NOT NULL, 
-	outflow Money NOT NULL, 
-	largest_payment Money NOT NULL,
-	largest_charge Money NOT NULL,
-	ave_trans_size Money NOT NULL,
+	inflow NUMERIC NOT NULL, 
+	outflow NUMERIC NOT NULL, 
+	largest_payment NUMERIC NOT NULL,
+	largest_charge NUMERIC NOT NULL,
+	ave_trans_size NUMERIC NOT NULL,
 	day_of_transactions timestamp NOT NULL,
 
 	CONSTRAINT day_user unique(user_name,day_of_transactions)
